34e83b209e9508e4898bab2dd93661797a67295d
bhuztez <bhuztez@gmail.com>
2018-12-27 18:10:33 +0800

    add AOJ ITP2_6_A ITP2_6_C ITP2_6_D

---
+++ solutions/AOJ/ITP2/ITP2_6_A.rs
@@ -0,0 +1,15 @@
+#[macro_use]
+extern crate porus;
+prelude!();
+
+fn main() {
+    let n: usize = read!();
+    let a = &mut Array::<usize>::new_from_iter((0..n).map(|_| read!()));
+    let q: usize = read!();
+
+    for _ in 0..q {
+        let k: usize = read!();
+        let r = list::bsearch(a, &k);
+        writelnf!("{:d}", if r.1 > r.0 { 1 } else { 0 });
+    }
+}
