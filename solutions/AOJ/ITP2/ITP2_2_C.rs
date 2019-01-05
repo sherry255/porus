8eb8ee8dceabed30e7312d46d852505429b78295
bhuztez <bhuztez@gmail.com>
2019-01-06 01:33:59 +0800

    add AOJ ITP2_2_C

---
+++ solutions/AOJ/ITP2/ITP2_2_C.rs
@@ -0,0 +1,26 @@
+#[macro_use]
+extern crate porus;
+prelude!();
+
+fn main() {
+    let n: usize = read!();
+    let q: usize = read!();
+
+    let a = &mut Array::<_>::new_from_iter((0..n).map(|_| DHeap::new(2, Array::<_>::new(), PartialOrd::gt)));
+
+    for _ in 0..q {
+        let op: usize = read!();
+        let t: usize = read!();
+        let h = list::get_mut(a, t);
+        if op == 0 {
+            let x: isize = read!();
+            heap::insert(h, x);
+        } else if op == 1 {
+            if let Some(&x) = Heap::top(h) {
+                writelnf!("{:d}", x)
+            }
+        } else if op == 2 {
+            Heap::extract(h);
+        }
+    }
+}
