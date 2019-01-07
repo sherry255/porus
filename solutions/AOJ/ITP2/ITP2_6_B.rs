8dbcce80dd488b876aa0bed89aef9c2ba1d068d8
bhuztez <bhuztez@gmail.com>
2019-01-08 01:41:41 +0800

    add AOJ ITP2_6_B

---
+++ solutions/AOJ/ITP2/ITP2_6_B.rs
@@ -0,0 +1,39 @@
+#[macro_use]
+extern crate porus;
+prelude!();
+
+fn includes(mut a: impl Iterator<Item=isize>, b: impl Iterator<Item=isize>) -> bool {
+    let mut x = 0;
+    for y in b {
+        loop {
+            if let Some(z) = a.next() {
+                x = z;
+            } else {
+                return false;
+            }
+
+            if y < x {
+                return false;
+            }
+
+            if y == x {
+                break;
+            }
+        }
+    }
+
+    return true;
+}
+
+fn main() {
+    let n: usize = read!();
+    let a = &mut Array::<isize>::new_from_iter((0..n).map(|_| read!()));
+    let m: usize = read!();
+    let b = &mut Array::<isize>::new_from_iter((0..m).map(|_| read!()));
+
+    if includes(list::iter(a),list::iter(b)) {
+        writelnf!("1");
+    } else {
+        writelnf!("0");
+    }
+}
